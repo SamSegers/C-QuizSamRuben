7c2469ee60af7a4ccac4ed4a46e6027b5b0e6b1f
RubenVictor <rvversan@avans.nl>
2015-10-27 14:48:45 +0100

    Db added 2.0
    asdf

--- EindopdrachtProg5RubenSam/EindopdrachtProg5RubenSam/Model/Vraag.cs
+++ EindopdrachtProg5RubenSam/EindopdrachtProg5RubenSam/Model/Vraag.cs
@@ -6,7 +6,7 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-
+//test
 namespace EindopdrachtProg5RubenSam.Model
 {
     using System;
