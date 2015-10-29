c716e053c25d1635b8903116293fb83058d9db4c
vegatarier <sam.sam.s@hotmail.com>
2015-10-29 12:48:19 +0100

    Models

---
+++ EindopdrachtProg5RubenSam/EindopdrachtProg5RubenSam/Models/Antwoord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EindopdrachtProg5RubenSam.Models
+{
+    class Antwoord
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public bool IsCorrect { get; set; }
+
+    }
+}
